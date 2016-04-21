00458299957184cc2a72a244434cf023366a2f75
Pjotr Savitski <gnum@tlu.ee>
2016-04-21 14:12:52 +0300

    Added secret to config file.
    This secret should not be private as it will only be used in
    development. The config should soon be changed so that it would be able
    to load correct values based on current invironment.

--- config/config.js
+++ config/config.js
@@ -1,7 +1,9 @@
+"use strict";
 module.exports = {
   app: {
     port: 3000,
     env: 'development',
+    secret: 'eX0u5tsuUydDDtUSHbnef1EE',
     db: {
       uri: 'mongodb://localhost/timeliner',
       options: {}
