19b7b376638ca7033e025cd3d8b8b3d9b605ca0a
Pjotr Savitski <gnum@tlu.ee>
2016-05-05 15:04:52 +0300

    Resolves #16, added error handler based on linked example.
    The error handler will try to make sure that API endpoints always
    respond with JSON, evene if errors are thrown. It will also allow the
    message to be sent within the response in case the status code was
    provided (this should indicate an intention), otherwise, the negeral
    code 500 and JSON payload with statdard message would be used. This
    implementation could change in future.

--- app/routes/api/index.js
+++ app/routes/api/index.js
@@ -9,13 +9,27 @@ module.exports = function (app) {
 
   //TODO cors should be only on api, not app
   app.use(cors());
-  /*apiRouter.use(function *(next) {
-    this.type = 'json';
-    return yield next;
-  });*/
 
   require(__dirname + '/hello')(apiRouter);
   require(__dirname + '/auth')(apiRouter);
+
+  apiRouter.use(function *(next) {
+    try {
+      yield next;
+    } catch (err) {
+      this.status = err.status || 500;
+      let responseBody = { message: 'internal_server_error' };
+      if ( err.status ) {
+        if ( typeof err.message === 'object' ) {
+          responseBody = err.message;
+        } else {
+          responseBody.message = err.message;
+        }
+      }
+      this.body = responseBody;
+      this.app.emit('error', err, this);
+    }
+  });
 
   app.use(apiRouter.routes());
   app.use(apiRouter.allowedMethods());
