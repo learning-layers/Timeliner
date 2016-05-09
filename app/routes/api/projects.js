36edf2b695583184c0e90132e4e2afd9e3a87db2
Pjotr Savitski <gnum@tlu.ee>
2016-05-09 18:25:21 +0300

    References #17, added project creation logic.
    It still has some issues and woul fail miserably in case end data was
    missing. Will be changed soon enough.

--- app/routes/api/projects.js
+++ app/routes/api/projects.js
@@ -17,8 +17,28 @@ module.exports = function (apiRouter) {
     this.throw(501, 'not_implemented');
   });
 
-  projectRouter.post('/', auth.ensureAuthenticated, auth.ensureUser, function *() {
-    this.throw(501, 'not_implemented');
+  projectRouter.post('/', auth.ensureAuthenticated, auth.ensureUser, bodyParser, function *() {
+    try {
+      let project = new Project({
+        title: this.request.body.title,
+        start: new Date(this.request.body.start),
+        end: new Date(this.request.body.end),
+        creator: this.user._id,
+        participants: [this.user._id]
+      });
+
+      project = yield project.save();
+
+      this.status = 200;
+      this.body = {
+        data: {
+          project: project
+        }
+      };
+    } catch(err) {
+      console.log('Create project', err);
+      this.throw(500, 'creation_failed');
+    }
   });
 
   projectRouter.put('/:project', auth.ensureAuthenticated, auth.ensureUser, function *() {
