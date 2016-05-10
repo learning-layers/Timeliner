18133cb2e3bc7edd8649b1a99f5211c075de35e4
Pjotr Savitski <gnum@tlu.ee>
2016-05-10 13:01:05 +0300

    References #17, implemented listing of own projects.
    Also added some prepopulation logic to these endpoints. Still needs to
    be refined.

--- app/routes/api/projects.js
+++ app/routes/api/projects.js
@@ -10,7 +10,20 @@ module.exports = function (apiRouter) {
   var projectRouter = new Router({ prefix: '/projects' });
 
   projectRouter.get('/', auth.ensureAuthenticated, auth.ensureUser, function *() {
-    this.throw(501, 'not_implemented');
+    try {
+      // TODO Needs a bit more control over what kind of data is populated
+      // Full user objects might not be needed. It could be enough to get:
+      // _id, name and email (not sure of last one is really needed here)
+      // Limiting amout of loaded data should be a wise choice
+      const projects = yield Project.find({ participants: this.user._id }).populate('creator owner participants').exec();
+      this.status = 200;
+      this.body = {
+        data: projects
+      };
+    } catch(err) {
+      console.log(err);
+      this.throw(500, 'internal_server_error');
+    }
   });
 
   projectRouter.get('/:project', auth.ensureAuthenticated, auth.ensureUser, function *() {
@@ -43,12 +56,15 @@ module.exports = function (apiRouter) {
       });
 
       project = yield project.save();
+      // TODO Needs a bit more control over what kind of data is populated
+      // Full user objects might not be needed. It could be enough to get:
+      // _id, name and email (not sure of last one is really needed here)
+      // Limiting amout of loaded data should be a wise choice
+      project = yield Project.populate(project, 'creator owner participants');
 
       this.status = 200;
       this.body = {
-        data: {
-          project: project
-        }
+        data: project
       };
     } catch(err) {
       console.log('Create project', err);
