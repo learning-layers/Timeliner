57a2a61c242b7b5082bcfe258324b994e3c4c888
Pjotr Savitski <gnum@tlu.ee>
2016-09-02 16:40:43 +0300

    Updated mongoose and replaces Promises.
    Updated mongoose to the latest version. Replaced default promise library
    with native promises (should probably switch to Bluebird instead).
    NB! Please update mongoose with npm.

--- app/db.js
+++ app/db.js
@@ -3,8 +3,11 @@
 const mongoose = require('mongoose');
 
 module.exports = function(appConfig) {
+  // TODO Consider replacing this with Bluebird
+  mongoose.Promise = global.Promise;
+
   mongoose.connect(appConfig.app.db.uri, appConfig.app.db.options);
   mongoose.connection.on('error', function(err) {
-    console.log('Database error', err);
+    console.error('Database error', err);
   });
 };
