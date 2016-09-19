c8d6573d164f8bc58e3df7c4ff7fbf789e684b45
Pjotr Savitski <gnum@tlu.ee>
2016-09-19 15:44:00 +0300

    Changed MongoDB connection a bit.
    This should make it a bit more "talkative".

--- app/db.js
+++ app/db.js
@@ -6,7 +6,19 @@ module.exports = function(appConfig) {
   mongoose.Promise = require('bluebird');
 
   mongoose.connect(appConfig.app.db.uri, appConfig.app.db.options);
-  mongoose.connection.on('error', function(err) {
-    console.error('Database error', err);
+
+  mongoose.connection.on('connected', function () {
+    console.log('Mongoose default connection open to ' + appConfig.app.db.uri);
   });
+
+  mongoose.connection.on('error', function(err) {
+    console.error('Mongoose default connection error: ', err);
+  });
+
+  process.on('SIGINT', function() {
+  mongoose.connection.close(function () {
+    console.log('Mongoose default connection disconnected through app termination');
+    process.exit(0);
+  });
+});
 };
